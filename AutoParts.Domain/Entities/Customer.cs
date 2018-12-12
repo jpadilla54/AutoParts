0a2469f456f89a36327b9187ffce288398efb7e6
daniel <daniel_maltez94@hotmail.com>
2018-12-12 00:26:35 -0600

    CustomerTest

---
+++ AutoParts.Domain/Entities/Customer.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoParts.Domain.Entities
+{
+    public class Customer
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Telephone { get; set; }
+
+
+        public Customer(int id, string name, string address, int telephone)
+        {
+
+            Id = id;
+            Name = name;
+            Address = address;
+            Telephone = telephone;
+
+
+        }
+
+
+
+    }
+}
---
+++ AutoParts.Test/CustomerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AutoParts.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoParts.Test
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void shouldCreateCustomer()
+        {
+            Customer customer = new Customer(1, "Daniel", "Rivera Hernandez", 99716584);
+            Assert.IsNotNull(customer);
+            Assert.AreEqual(1, customer.Id);
+        }
+
+
+    }
+}
